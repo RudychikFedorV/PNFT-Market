cfd20bbddef4b1f60de5770ebc03880663c46a59
RudychikFedorV <rudychik.fedor@gmail.com>
2023-12-07 23:53:57 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthHeaderComponent } from './auth-header/auth-header.component';
+import { AuthFooterComponent } from './auth-footer/auth-footer.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { AuthImgComponent } from './auth-img/auth-img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthHeaderComponent,
+    AuthFooterComponent,
+    AuthFormComponent,
+    AuthImgComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/auth-img/auth-img.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-img',
+  templateUrl: './auth-img.component.html',
+  styleUrls: ['./auth-img.component.scss']
+})
+export class AuthImgComponent {
+
+}
